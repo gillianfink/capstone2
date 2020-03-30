21761a454a4946e52ca31d3045218b730865dd1b
gillianfink <36948522+gillianfink@users.noreply.github.com>
2020-03-29 22:07:55 -0500

    Add files via upload

---
+++ Capstone Build 3:28/convoscript.js
@@ -0,0 +1,101 @@
+$(document) .ready(function() {
+  
+  
+  
+     var termclick= true;
+    $("#termA").click(function(){
+        if (termclick==true) {
+            
+            $("#termA").css("top", "19%");
+            $(this).unbind("mouseenter mouseleave");
+            termclick=false;
+        }
+        else {
+            $("#termA").css("top", "43%");
+            
+            termclick=true;
+        }    
+    });
+  
+
+
+  
+  
+  
+       var termBclick= true;
+    $(".term").click(function(){
+        if (termBclick==true) {
+          
+          $( "#termB" ).animate({
+              opacity: "1"
+              }, 200);
+            
+            $("#termB").css("top", "61%");
+            $("#termB").css("visibility", "visible");
+            termBclick=false;
+        }
+        else {
+          
+          $( "#termB" ).animate({
+              opacity: "0"
+              }, 200);
+          
+            $("#termB").css("top", "35%");
+            $("#termB").css("visibility", "hidden");
+            
+            termBclick=true;
+        }    
+    });
+  
+         var di= true;
+    $(".term").click(function(){
+        if (di==true) {
+          
+          $( ".dialect" ).animate({
+              opacity: "1"
+              }, 800);
+      
+            di=false;
+        }
+        else {
+          
+          $( ".dialect" ).animate({
+              opacity: "0"
+              }, 800);
+          
+           
+            di=true;
+        }    
+    });
+  
+  
+  
+  
+
+  jQuery(function( $ ) {
+    var keymap = {};
+
+    // LEFT
+    keymap[ 37 ] = "#prev";
+    // RIGHT
+    keymap[ 39 ] = "#next";
+
+    $( document ).on( "keyup", function(event) {
+        var href,
+            selector = keymap[ event.which ];
+        // if the key pressed was in our map, check for the href
+        if ( selector ) {
+            href = $( selector ).attr( "href" );
+            if ( href ) {
+                // navigate where the link points
+                window.location = href;
+            }
+        }
+    });
+});
+  
+  
+
+  
+  
+}); 
